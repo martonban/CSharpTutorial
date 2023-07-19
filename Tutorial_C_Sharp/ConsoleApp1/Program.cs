5b9a2ec666d16714d89ff91c09208b15ac08d77f
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-07-19 09:26:04 +0200

    Type Casting

--- Tutorial_C_Sharp/ConsoleApp1/Program.cs
+++ Tutorial_C_Sharp/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.ComponentModel;
 
 namespace Tutorial_C_Sharp
 {
@@ -7,8 +8,20 @@ namespace Tutorial_C_Sharp
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.Beep();
+
+            double a = 3.14;
+            int b = Convert.ToInt32(a);
+
+            Console.WriteLine(b);
+            Console.WriteLine(a.GetType());
+
+            int c = 123;
+            double d = Convert.ToDouble(c) + 0.1;
+
+            Console.WriteLine(d);
+
+            Console.ReadKey();
+            
         }
     }
 }
