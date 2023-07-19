1f9d7ded97645ce868b861fe1445dbaa83165bc3
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-07-19 12:37:05 +0200

    Simple Random Number Order Guessing game
    Just a simple game to practice all I saw in the tutorial. Not perfect at all!!!!!

--- Tutorial_C_Sharp/ConsoleApp1/Program.cs
+++ Tutorial_C_Sharp/ConsoleApp1/Program.cs
@@ -4,52 +4,61 @@ using System.ComponentModel;
 
 namespace Tutorial_C_Sharp
 {
-    class Program { 
-        
+    class Program {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Type me a number: ");
-            String number = Console.ReadLine();
+            Console.WriteLine("Game Rules");
+            Console.WriteLine("Every round you will recive a random number. After you get the random number you need to guess the place. The number is going to stored in a list after all the ten round you will see, you gess the order well or not");
 
-            switch (number) 
-            {
-                case "one":
-                    Console.WriteLine("It's a number");
-                    break;
-                case "two":
-                    Console.WriteLine("It's a number");
-                    break;
-                case "three":
-                    Console.WriteLine("It's a number");
-                    break;
-                case "four":
-                    Console.WriteLine("It's a number");
-                    break;
-                case "five":
-                    Console.WriteLine("It's a number");
-                    break;
-                case "six":
-                    Console.WriteLine("It's a number");
-                    break;
-                case "seven":
-                    Console.WriteLine("It's a number");
-                    break;
-                case "eight":
-                    Console.WriteLine("It's a number");
-                    break;
-                case "nine":
-                    Console.WriteLine("It's a number");
-                    break;
-                case "ten":
-                    Console.WriteLine("It's a number");
-                    break;
-                default:
-                    Console.WriteLine("It's not a number");
-                    break;
+            int[] numbers = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int count = 0;
+            Random rnd = new Random();
+
+            while (count < 10) {
+
+
+                int number = rnd.Next(1,100);
+
+                Console.Write("Your number is: ");
+                Console.WriteLine(number);
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("List NOW");
+
+                printList(numbers);
+
+                Console.WriteLine("What is your guess?:");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                
+                numbers[guess] = number;
+                count++;
             }
 
+            if (isThisInOrder(numbers))
+            {
+                Console.WriteLine("VICTORY!!!");
+            }
+            else {
+                Console.WriteLine("DEFEAT!!");
+            }
+
+
             Console.ReadKey();
+        }
+
+        static void printList(int[] array) {
+            for (int i = 0; i < array.Length; i++) {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        static bool isThisInOrder(int[] array) {
+            for (int i = 0; i < array.Length-1; i++) {
+                if (array[i] > array[i + 1]) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
