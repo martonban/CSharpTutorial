630f0071a2295239060a1e2fa0d625cc6fa3d699
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-07-19 10:04:13 +0200

    switch

--- Tutorial_C_Sharp/ConsoleApp1/Program.cs
+++ Tutorial_C_Sharp/ConsoleApp1/Program.cs
@@ -9,18 +9,46 @@ namespace Tutorial_C_Sharp
         static void Main(string[] args)
         {
 
-            Random random = new Random();
+            Console.WriteLine("Type me a number: ");
+            String number = Console.ReadLine();
 
-            // Random Int
-            random.Next();
+            switch (number) 
+            {
+                case "one":
+                    Console.WriteLine("It's a number");
+                    break;
+                case "two":
+                    Console.WriteLine("It's a number");
+                    break;
+                case "three":
+                    Console.WriteLine("It's a number");
+                    break;
+                case "four":
+                    Console.WriteLine("It's a number");
+                    break;
+                case "five":
+                    Console.WriteLine("It's a number");
+                    break;
+                case "six":
+                    Console.WriteLine("It's a number");
+                    break;
+                case "seven":
+                    Console.WriteLine("It's a number");
+                    break;
+                case "eight":
+                    Console.WriteLine("It's a number");
+                    break;
+                case "nine":
+                    Console.WriteLine("It's a number");
+                    break;
+                case "ten":
+                    Console.WriteLine("It's a number");
+                    break;
+                default:
+                    Console.WriteLine("It's not a number");
+                    break;
+            }
 
-            // Random int between parameters
-            random.Next(1, 7);
-
-            // Random Double 0-1
-            double rnd = random.NextDouble();
-
-            Console.WriteLine(rnd);
             Console.ReadKey();
         }
     }
