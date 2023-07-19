532b1149ace7022b88ffb52ecc9ee8572414d81e
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-07-19 13:09:36 +0200

    params keyword

--- Tutorial_C_Sharp/ConsoleApp1/Program.cs
+++ Tutorial_C_Sharp/ConsoleApp1/Program.cs
@@ -7,58 +7,21 @@ namespace Tutorial_C_Sharp
     class Program {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Game Rules");
-            Console.WriteLine("Every round you will recive a random number. After you get the random number you need to guess the place. The number is going to stored in a list after all the ten round you will see, you gess the order well or not");
-
-            int[] numbers = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            int count = 0;
-            Random rnd = new Random();
-
-            while (count < 10) {
-
-
-                int number = rnd.Next(1,100);
-
-                Console.Write("Your number is: ");
-                Console.WriteLine(number);
-                Console.WriteLine("-------------------------");
-                Console.WriteLine("List NOW");
-
-                printList(numbers);
-
-                Console.WriteLine("What is your guess?:");
-                int guess = Convert.ToInt32(Console.ReadLine());
-                
-                numbers[guess] = number;
-                count++;
-            }
-
-            if (isThisInOrder(numbers))
-            {
-                Console.WriteLine("VICTORY!!!");
-            }
-            else {
-                Console.WriteLine("DEFEAT!!");
-            }
-
-
+            double total = CheckOut(3.99, 5.75, 15);
+            Console.WriteLine(total);
             Console.ReadKey();
         }
 
-        static void printList(int[] array) {
-            for (int i = 0; i < array.Length; i++) {
-                Console.WriteLine(array[i]);
+        static double CheckOut(params double[] prices) {
+            double total = 0;
+
+            foreach (double price in prices)
+            {
+                total += price;
             }
+
+            return total;
         }
 
-        static bool isThisInOrder(int[] array) {
-            for (int i = 0; i < array.Length-1; i++) {
-                if (array[i] > array[i + 1]) {
-                    return false;
-                }
-            }
-            return true;
-        }
     }
 }
