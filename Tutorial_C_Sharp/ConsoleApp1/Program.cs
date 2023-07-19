52df425352e1d3b3e7467c66c2516cceb9022322
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-07-19 14:00:42 +0200

    objects + constructors

--- Tutorial_C_Sharp/ConsoleApp1/Program.cs
+++ Tutorial_C_Sharp/ConsoleApp1/Program.cs
@@ -8,9 +8,42 @@ namespace Tutorial_C_Sharp
     class Program {
         static void Main(string[] args)
         {
+            Human human1 = new Human("Rick", 65);
+            Human human2 = new Human("Morty", 16);
 
-            Messages.Hello();
+            human1.Eat();
+            human1.Sleep();
+
+            human2.Eat();
+            human2.Sleep();
+
             Console.ReadKey();
         }
     }
+
+
+
+    class Human {
+
+        public String name;
+        public int age;
+
+
+        public Human(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void Eat() {
+            Console.WriteLine(name + " is eating");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
+    
+    }
+
 }
